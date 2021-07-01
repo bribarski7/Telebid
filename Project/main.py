6ed47e47e3e5877e6ac607ed2e61011b89379692
ribarski <bribarski7@gmail.com>
2021-07-01 15:01:45 +0300

    New Database

--- Project/main.py
+++ Project/main.py
@@ -1,19 +1,30 @@
 from flask import Flask, request, render_template
+import sqlite3
 
 # Flask constructor
 app = Flask(__name__)
 
-# A decorator used to tell the application
-# which URL is associated function
-@app.route('/', methods =["GET", "POST"])
+
+con = sqlite3.connect('database.db', check_same_thread=False)
+cur = con.cursor()
+#cur.execute('''CREATE TABLE Sum(num1 real, num2 real, sum real)''')
+
+@app.route('/', methods=["GET", "POST"])
 def gfg():
     if request.method == "POST":
-       # getting input with name = fname in HTML form
-       first_number = request.form.get("fn")
-       # getting input with name = lname in HTML form
-       second_number = request.form.get("sn")
-       return str(int(first_number) + int(second_number))
+        first_number = request.form.get("fn")
+        second_number = request.form.get("sn")
+        sum = int(first_number) + int(second_number)
+
+        cur.execute('insert into Sum (num1, num2, sum) values (' + str(first_number) + ',' + str(second_number) + ',' + str(sum) + ')')
+        con.commit()
+        cur.execute('''select * from Sum''')
+        data = cur.fetchall()
+        for x in data:
+            print(x)
+        return str(int(sum))
     return render_template("Main.html")
 
-if __name__=='__main__':
-   app.run()
+
+if __name__ == '__main__':
+    app.run()
