c85c0f29f6e79339b29adf855ec49d3b7261f3f6
ribarski <bribarski7@gmail.com>
2021-07-08 11:32:11 +0300

    adding files

--- Project/main.py
+++ Project/main.py
@@ -9,7 +9,9 @@ con = sqlite3.connect('database.db', check_same_thread=False)
 cur = con.cursor()
 #cur.execute('''CREATE TABLE Sum(num1 real, num2 real, sum real)''')
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
+
+@app.route('/sum', methods=["GET", "POST"])
 def gfg():
     if request.method == "POST":
         first_number = request.form.get("fn")
@@ -18,12 +20,32 @@ def gfg():
 
         cur.execute('insert into Sum (num1, num2, sum) values (' + str(first_number) + ',' + str(second_number) + ',' + str(sum) + ')')
         con.commit()
-        cur.execute('''select * from Sum''')
+        cur.execute('''select Count(*) from Sum''')
         data = cur.fetchall()
-        for x in data:
-            print(x)
-        return str(int(sum))
-    return render_template("Main.html")
+        data_str = str(data[0])
+        return data_str[1:-2] + ":" + str(sum)
+    return render_template("login.html")
+
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+    return render_template('login.html', msg = msg)
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        msg += "Hello "
+        msg += username
+        return render_template('login.html', msg = msg)
+    return render_template('register.html', msg = msg)
 
 
 if __name__ == '__main__':
