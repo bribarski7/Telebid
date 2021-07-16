e6dea3361e61145ac99f894f357a8086160c0862
ribarski <bribarski7@gmail.com>
2021-07-16 12:07:54 +0300

    final

--- Project/main.py
+++ Project/main.py
@@ -9,33 +9,35 @@ con = sqlite3.connect('database.db', check_same_thread=False)
 cur = con.cursor()
 #cur.execute('''CREATE TABLE Sum(num1 real, num2 real, sum real)''')
 #cur.execute('''DROP TABLE Accounts''')
-#cur.execute('''CREATE TABLE Accounts(username text, password text, email text)''')
-
+#cur.execute('''CREATE TABLE Accounts(username text NOT NULL, password text NOT NULL, email text NOT NULL,UNIQUE(username, email))''')
+#cur.execute('''DROP TABLE Uchenici''')
+cur.execute('''CREATE TABLE "Uchenici" (
+	"Godini"	INTEGER,
+	"Pol"	TEXT,
+	"uchilishte_id"	INTEGER,
+	FOREIGN KEY(uchilishte_id) REFERENCES Uchilishte(id)
+)''')
 @app.route('/')
 
 @app.route('/sum', methods=["GET", "POST"])
-def gfg():
-    if request.method == "POST":
-        first_number = request.form.get("fn")
-        second_number = request.form.get("sn")
-        sum = int(first_number) + int(second_number)
 
-        cur.execute('insert into Sum (num1, num2, sum) values (' + str(first_number) + ',' + str(second_number) + ',' + str(sum) + ')')
-        con.commit()
-        cur.execute('''select Count(*) from Sum''')
-        data = cur.fetchall()
-        data_str = str(data[0])
-        return data_str[1:-2] + ":" + str(sum)
-    return render_template("Main.html")
-
-
-@app.route('/login', methods =['GET', 'POST'])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
+        if username == '' or password == '':
+            return render_template('login.html', msg = "error logging in")
+        cur.execute('SELECT * FROM Accounts WHERE username = "{u1}" AND password = "{p1}"'.format(u1=username,p1=password, ))
+        account = cur.fetchone()
+        if account:
+            msg = username
+            return render_template('Main.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
     return render_template('login.html', msg = msg)
+
 
 @app.route('/register', methods =['GET', 'POST'])
 def register():
@@ -44,6 +46,8 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+        if username == '' or password == '' or email == '':
+            return render_template('register.html', msg = "error registering")
         execute = "insert into Accounts (username, password, email) values('{u1}','{p1}','{e1}')".format(u1 = username, p1 = password, e1 = email)
         cur.execute(execute)
         con.commit()
